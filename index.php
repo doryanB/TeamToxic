af1d8adc5fffbe46fe2025d63eaa18e54d9f334a
Alexandre BAR <alexandre.bar83@gmail.com>
2015-12-03 21:50:34 +0100

    Ajout de footer et header

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="style.css"/>
-    </head>
+<?php
+session_start();
+include 'header.html';
+?>
     <body>
         <div class="triangle"></div>
 
     </body>
-</html>
+<?php
+include 'footer.html';
+?>
