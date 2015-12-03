e8fc0c943842ef38b837796e6353a13b52dbdd1c
Alexandre BAR <alexandre.bar83@gmail.com>
2015-12-04 00:20:24 +0100

    Ca pèse un peu plus

--- index.php
+++ index.php
@@ -3,25 +3,62 @@ session_start();
 include 'header.html';
 ?>
 <body>
-<div class="triangle"></div>
-<div class="row">
+<nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Le super site de merde</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+</nav><!-- /.navbar -->
+
+
+
+
+
+
+<div class="container">
+
+    <div class="row row-offcanvas row-offcanvas-right">
+
+
+            <div class="jumbotron">
+            </div>
+
+
+
+
+
+
 <div><!--/.col-xs-12.col-sm-9-->
-    <div class="col-xs-6 col-lg-4">
+    <div class="col-md-2 col-md-offset-1 col-lg-4">
         <h2>Heading</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!--/.col-xs-6.col-lg-4-->
-    <div class="col-xs-6 col-lg-4">
+    <div class="col-md-3 col-md-offset-1 col-lg-4">
         <h2>Heading</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!--/.col-xs-6.col-lg-4-->
-    <div class="col-xs-6 col-lg-4">
+    <div class="col-md-6 col-md-offset-1 col-lg-4">
         <h2>Heading</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!--/.col-xs-6.col-lg-4-->
-    <div class="col-xs-6 col-lg-4">
+    <div class="col-md-6 col-md-offset-1 col-lg-4">
         <h2>Heading</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
