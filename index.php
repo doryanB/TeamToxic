b25ea45a0ee33228cfe7514f986f6b93016038ce
Alexis METGE <alexis.metge@etu.unice.fr>
2015-12-04 00:05:57 +0100

    Swap à gauche et droite géré !

--- index.php
+++ index.php
@@ -8,10 +8,37 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <link rel="stylesheet" type="text/css" href="style.css"/>
     </head>
     <body>
-        <div class="triangle"></div>
 
+        <p id="log"></p>
     </body>
 </html>
+<script>
+    var sensibility=200;
+    var touchStartX;
+    document.addEventListener("touchstart", function(event){
+        event.preventDefault();
+        touchStartX = event.targetTouches[0].pageX;
+    });
+    
+    document.addEventListener("touchend", function(event){
+        var tmp = event.changedTouches[0].pageX-touchStartX;
+        if(tmp>sensibility) swappedLeft();
+        else if(tmp<sensibility*-1) swappedRight();
+    });
+    
+    document.addEventListener("touchmove", function(event){
+        var tmp = event.targetTouches[0].pageX-touchStartX;
+        if(tmp<0) return;
+    }, false);
+    
+    function swappedLeft(){
+        console.log("Swapped left");
+    }
+    function swappedRight(){
+        console.log("Swapped right");
+    }
+</script>
