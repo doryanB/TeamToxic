455cfe0d60f03845cc95ef4cd2a30da190e614c6
Alexandre BAR <alexandre.bar83@gmail.com>
2015-12-03 23:17:15 +0100

    Ajout de bootstrap. SALUT !

--- index.php
+++ index.php
@@ -2,10 +2,34 @@
 session_start();
 include 'header.html';
 ?>
-    <body>
-        <div class="triangle"></div>
+<body>
+<div class="triangle"></div>
+<div class="row">
+<div><!--/.col-xs-12.col-sm-9-->
+    <div class="col-xs-6 col-lg-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div><!--/.col-xs-6.col-lg-4-->
+    <div class="col-xs-6 col-lg-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div><!--/.col-xs-6.col-lg-4-->
+    <div class="col-xs-6 col-lg-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div><!--/.col-xs-6.col-lg-4-->
+    <div class="col-xs-6 col-lg-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+    </div><!--/.col-xs-6.col-lg-4-->
 
-    </body>
+</div><!--/row-->
+</div><!--/.col-xs-12.col-sm-9-->
+</body>
 <?php
 include 'footer.html';
 ?>
